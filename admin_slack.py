3448e87acb7d1fb45131e167d22b5f9b8440bac7
Clément P <cpieri@student.42.fr>
2019-07-24 19:17:55 +0200

    add main

---
+++ admin_slack.py
@@ -0,0 +1,22 @@
+import sys
+from slack_token import token
+from clean_channel import *
+
+def	choose():
+	ask = """Welcome !! Here you can choose to perform actions easily on your slack :)\nChoose a number:
+	1) for delete all messages from specific channel
+	2) for exit\n"""
+	user_response = input(ask)
+	return (int(user_response))
+
+def exit():
+	print ('Good Bye my Friend !!')
+	sys.exit()
+
+if __name__ == "__main__":
+	response = choose()
+	if response == 1:
+		channel = input('Okay, Please enter the name of channel that you want clean:\t')
+		clean_channel(channel, token)
+	if response == 2:
+		exit()
--- clean_channel.py
+++ clean_channel.py
@@ -1,35 +1,27 @@
 import requests
-import time
-import sys
-from slack_token import token 
+from error import *
 
-if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		print ('Please enter a name of channel to clean !')
-		sys.exit()
+def clean_channel(channel2clean, token):
 	print ('Your token is: {token}'.format(token=token))
-	chan_to_find = sys.argv[1]
+	chan_to_find = channel2clean
 	response = requests.get('https://slack.com/api/channels.list?limit=100&token={token}'.format(token=token)).json()
 	if response['ok'] == False:
-		print ('Error: {error}'.format(error=response['error']))
-		sys.exit()
+		error('Error: {error}'.format(error=response['error']))
 	chan_id = False
 	for channel in response['channels']:
 		if channel['name'] == chan_to_find:
 			chan_id = channel['id']
 			print ('The channel "{chan}" have the id "{id}"'.format(chan=chan_to_find, id=chan_id))
 	if chan_id == False:
-		print ('Error: Your channel doesn\'t exist !')
-		sys.exit()
+		error('Error: Your channel doesn\'t exist !')
 	confirmation = input('Really want to delete all messages from this channel ? [y/n]')
 	if confirmation == 'y':
 		response = requests.get('https://slack.com/api/channels.history?count=1000&channel={id}&token={token}'.format(id=chan_id, token=token)).json()
 		if response['ok'] == False:
-			print ('Error: {error}'.format(error=response['error']))
-			sys.exit()
+			error('Error: {error}'.format(error=response['error']))
 		for msg in response['messages']:
 			ts = msg['ts']
 			res = requests.post('https://slack.com/api/chat.delete?ts={ts2del}&channel={chan_id}&token={token}'.format(ts2del=ts, chan_id=chan_id, token=token))
 			print (res)
 	else:
-		print ('Okay, you\'re not sure.')
+		error('Okay, you\'re not sure.')
